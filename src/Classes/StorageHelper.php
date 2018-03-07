e1e4e39cae202b255c84628aafe8c8ab89584110
LeebranZadwell <levon@clsimplex.com>
2018-03-07 15:44:40 -0800

    added StorageHelper

---
+++ src/Classes/StorageHelper.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace CLSimplex\Tuxedo\Helpers;
+
+use Illuminate\Support\Facades\Storage;
+
+/**
+ * StorageHelper is an attempt to provide additional
+ * helpers that can be moved outside of Monocle.
+ *
+ * @author Levon Zadravec-Powell levon@clsimplex.com
+ * @since  x.x.x
+ */
+class StorageHelper {
+  /**
+   * @since  x.x.x
+   * @codeCoverageIgnore
+   * @param  string $file_name
+   * @return bool
+   */
+  public static function has_local_file(string $file_name) {
+    return Storage::disk('local')->exists($file_name);
+  }
+
+  /**
+   * @since  x.x.x
+   * @codeCoverageIgnore
+   * @param  string $file_name
+   * @param  mixed  $contents
+   * @return void
+   */
+  public static function put_into_local_file(string $file_name, $contents) {
+    Storage::disk('local')->put($file_name, $contents);
+  }
+
+  /**
+   * Silent failure pattern.
+   *
+   * @since  x.x.x
+   * @codeCoverageIgnore
+   * @param  string $file_name
+   * @return void
+   */
+  public static function delete_local_file(string $file_name) {
+    if ( static::has_local_file($file_name) ) {
+      Storage::delete($file_name);
+    }
+  }
+
+  /**
+   * "Guarantees" file creation - so you'll get something,
+   * whether it is an empty string, or existing content.
+   * So in general, call this only if you plan on using the
+   * file you just created.
+   *
+   * @since  x.x.x
+   * @codeCoverageIgnore
+   * @param  string $file_name
+   * @return string
+   */
+  public static function get_local_file(string $file_name) {
+    if ( ! static::has_local_file($file_name) ) {
+      Storage::disk('local')->put( $file_name, '' );
+    }
+
+    return Storage::disk('local')->get($file_name);
+  }
+
+  /**
+   * @since  x.x.x
+   * @codeCoverageIgnore
+   * @param  string $file_name
+   * @return string
+   */
+  public static function get_local_file_path(string $file_name) {
+    return storage_path('app/' . $file_name);
+  }
+}
